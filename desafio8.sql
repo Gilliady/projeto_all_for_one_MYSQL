55096264468431a7064746c87a0bbfbfd3084bd1
Gilliady <gilliadyazevedo@gmail.com>
2023-07-05 15:59:51 -0300

    agora vai

--- desafio8.sql
+++ desafio8.sql
@@ -1,4 +1,4 @@
 SELECT
 CONCAT(first_name, ' ', last_name) AS 'full_name',
-CONCAT(city, '-', state_province, ' ', address) as 'location'
+CONCAT(city, '-', state_province, ', ', address) as 'location'
 FROM employees;
